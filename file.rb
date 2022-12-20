729a47d4688170de78a15a996ec75e5cd23cd6e2
vflorin1995 <vflorin1995@gmail.com>
2022-12-20 17:28:53 +0200

    Fix linters

--- file.rb
+++ file.rb
@@ -1,36 +1,39 @@
 class Nameble
   def correct_name
-    return NotImplementedError
+    NotImplementedError
   end
 end
 
 class Decorator < Nameble
   def initialize(nameble)
+    super()
     @nameble = nameble
   end
+
   def correct_name
-    return @nameble.correct_name
+    @nameble.correct_name
   end
 end
 
 class CapitalizeDecorator < Decorator
   def correct_name
-    return @nameble.correct_name.upcase
+    @nameble.correct_name.upcase
   end
 end
 
 class TrimmerDecorator < Decorator
   def correct_name
     if @nameble.correct_name.length > 10
-      return @nameble.correct_name[0,10]
+      @nameble.correct_name[0, 10]
     else
-      return @nameble.correct_name
+      @nameble.correct_name
     end
   end
 end
 
 class Person < Nameble
   def initialize(age, name = 'Unknown', permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -45,7 +48,7 @@ class Person < Nameble
   end
 
   def correct_name
-    return @name
+    @name
   end
 
   private
