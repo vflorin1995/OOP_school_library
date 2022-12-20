5103118c3bc5ec4ecfb4bd67a0c2de6956c3139a
vflorin1995 <vflorin1995@gmail.com>
2022-12-20 17:18:42 +0200

    Add decorator

--- file.rb
+++ file.rb
@@ -1,4 +1,35 @@
-class Person
+class Nameble
+  def correct_name
+    return NotImplementedError
+  end
+end
+
+class Decorator < Nameble
+  def initialize(nameble)
+    @nameble = nameble
+  end
+  def correct_name
+    return @nameble.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    return @nameble.correct_name.upcase
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameble.correct_name.length > 10
+      return @nameble.correct_name[0,10]
+    else
+      return @nameble.correct_name
+    end
+  end
+end
+
+class Person < Nameble
   def initialize(age, name = 'Unknown', permission: true)
     @id = Random.rand(1..1000)
     @name = name
@@ -11,6 +42,10 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    return @name
   end
 
   private
